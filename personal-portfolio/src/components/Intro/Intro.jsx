ba58bdb5940a1271caa4ac65c93fd5ef974021be
unknown <j.chen1904@gmail.com>
2025-07-23 02:22:34 -0400

    social media links

--- personal-portfolio/src/components/Intro/Intro.jsx
+++ personal-portfolio/src/components/Intro/Intro.jsx
@@ -11,9 +11,20 @@ export const Intro = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Hey, I'm Jessica!</h1>
             <p className={styles.description}>I am computer science student at the University of Toronto passionate about machine learning, game development, and computer graphics.</p>
-            <div className={styles.links}>
-              <a href={resume} target="_blank" rel="noopener noreferrer">Resume</a>
-            </div>
+            <ul className={styles.links}>
+              <li>
+                <a href="mailto:jiajing.chen@mail.utoronto.ca">Email</a>
+              </li>
+              <li>
+                <a href={resume} target="_blank" rel="noopener noreferrer">Resume</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/jessicachen-cjj/" target="_blank">LinkedIn</a>
+              </li>
+              <li>
+                <a href="https://github.com/swannercjj" target="_blank">GitHub</a>
+              </li>
+            </ul>
             {/* cv/resume, github, linkedin, email */}
         </div>
           <img src={getImageUrl("intro/profile_img.jpg")} alt="professional photo" className={styles.profileImg}/>
