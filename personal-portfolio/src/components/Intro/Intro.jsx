6d0f8b5df793f84ff9aa86e2012ff190f04f262f
unknown <j.chen1904@gmail.com>
2025-07-23 01:47:32 -0400

    resume

--- personal-portfolio/src/components/Intro/Intro.jsx
+++ personal-portfolio/src/components/Intro/Intro.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import styles from "./Intro.module.css"
 import { getImageUrl } from '../../utils';
+import resume from "../../resources/jiajing_chen_resume.pdf"
 
 
 export const Intro = () => {
@@ -10,6 +11,9 @@ export const Intro = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Hey, I'm Jessica!</h1>
             <p className={styles.description}>I am computer science student at the University of Toronto passionate about machine learning, game development, and computer graphics.</p>
+            <div className={styles.links}>
+              <a href={resume} target="_blank" rel="noopener noreferrer">Resume</a>
+            </div>
             {/* cv/resume, github, linkedin, email */}
         </div>
           <img src={getImageUrl("intro/profile_img.jpg")} alt="professional photo" className={styles.profileImg}/>
