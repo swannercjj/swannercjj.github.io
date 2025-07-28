bf964e283ae4f667d88d18bd4284fc293d9d1725
unknown <j.chen1904@gmail.com>
2025-07-27 21:22:38 -0400

    contact footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,52 @@
 import React from 'react'
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { IoMail, IoCodeSlashSharp } from "react-icons/io5";
+
+import styles from "./Contact.module.css"
 
 export const Contact = () => {
+  const links = [
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/in/jessicachen-cjj/",
+      icon: <FaLinkedin />
+    },
+    {
+      name: "Github",
+      href: "https://github.com/swannercjj",
+      icon: <FaGithub />
+    },
+    {
+      name: "Email",
+      href: "mailto:jiajing.chen@mail.utoronto.ca",
+      icon: <IoMail />
+    },
+    {
+      name: "Repo",
+      href: "https://github.com/swannercjj/swannercjj.github.io",
+      icon: <IoCodeSlashSharp />
+    }
+  ]
+
   return (
-    <div>Contact</div>
+    <footer id="contact">
+      <div className={styles.container}>
+        <ul className={styles.linksList}>
+            {links.map((link, index) => (
+              <li>
+                <a 
+                  className={styles.linkItem}
+                  key={index}
+                  href={link.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {link.icon}
+                </a>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,39 +9,40 @@ export const Navbar = () => {
 
     const [menuOpen, setMenuOpen] = useState(false);
 
-    return (<nav className={styles.navbar}>
-        <a className={styles.title} href="/">
-            Portfolio
-        </a>
-        <div className={styles.menu}>
-            <img 
-                className={styles.menuBtn} 
-                src={
-                    menuOpen 
-                        ? menuClose
-                        : menuIcon
-                } 
-                alt="menu-button"
-                onClick={() => setMenuOpen(!menuOpen)}
-            />
-            <ul 
-                className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-                onClick={() => setMenuOpen(false)}
-            >
-                <li>
-                    <a href="#about">About Me</a>
-                </li>
-                <li>
-                    <a href="#experience">Experience</a>
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    return (
+        <nav className={styles.navbar}>
+            <a className={styles.title} href="/">
+                Portfolio
+            </a>
+            <div className={styles.menu}>
+                <img 
+                    className={styles.menuBtn} 
+                    src={
+                        menuOpen 
+                            ? menuClose
+                            : menuIcon
+                    } 
+                    alt="menu-button"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                />
+                <ul 
+                    className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick={() => setMenuOpen(false)}
+                >
+                    <li>
+                        <a href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <a href="#experience">Experience</a>
+                    </li>
+                    <li>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 };
